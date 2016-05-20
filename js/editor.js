6cc20d659613c039317d821d2c0d172041cc7584
Andrew Gurung <andrewgurung@users.noreply.github.com>
2016-05-20 15:31:56 -0400

    Load notes data using $http service

--- js/editor.js
+++ js/editor.js
@@ -1,10 +1,15 @@
 // Editor controller
 // References App module to attach controller to this module
 angular.module('App')
-  .controller('EditorController', function($scope) {
-    // $scope service injected
-    // Creates model value and stores on $scope
-    $scope.state = {
-      editing: false
-    };
-  });
+.controller('EditorController', function($scope, $http) {
+  // $scope and $http services injected
+  // Creates model value and stores on $scope
+  $scope.editing = true;
+
+  // Load notes data using $http service
+  $http.get('/notes').success(function(data) {
+    $scope.notes = data;
+  }).error(function(err) {
+    $scope.error = 'Could not load notes';
+  })
+});
