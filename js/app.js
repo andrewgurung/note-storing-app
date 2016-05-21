8d690e5651a7cdb6feebd6b2890e1e6b87720db3
andrewgurung <andrewgurungg@gmail.com>
2016-05-21 09:07:26 -0400

    Custom markdown directive that converts plain text to Markdown

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 // Angular module named 'App'
 // which is linked to HTML element through ngApp directive
-angular.module('App', []);
+angular.module('App', [])
+.directive('markdown', function() { // markdown directive
+  // Creates Showdown converter to convert plain text to Markdown
+  var converter = new Showdown.converter();
+
+  // Return directive settings
+  return {
+    // Custom scope that expects value to be assigned to a markdown attribute
+    scope: {
+      markdown: '@'
+    },
+
+    // Use by angular as part of the rendering process
+    link: function(scope, element, attrs) {
+      // Scope watcher to update any model changes
+      scope.$watch('markdown', function() {
+        // Takes value inside `markdown` attribute and converts into markdown html
+        var content = converter.makeHtml(attrs.markdown);
+        // Injects converted content into the element where `markdown` attr is defined
+        element.html(content);
+      });
+    }
+  }
+});
